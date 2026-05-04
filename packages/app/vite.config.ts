ca8bb1e77d20d9d932832a6e73be79ae4b93b4d0
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2026-05-04 15:31:01 +1000

    add gh pages

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/aurelia-headless/' : '/',
   resolve: {
     alias: [
       { find: /^aurelia-headless$/, replacement: path.resolve(__dirname, '../components/src') },
